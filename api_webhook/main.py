1b1bb7e2ca786ab072713f0b6025d965755e3673
vitorh34 <vitorh34@yahoo.com.br>
2021-02-21 17:15:11 -0300

    teste

--- api_webhook/main.py
+++ api_webhook/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import json
 import base64
@@ -39,6 +39,17 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/mqttbroker/on_publish")
-def print_content(item: Item):
-    print(base64.b64decode(item))
+async def print_content(item: Item, request: Request):
+    print(item)
+    print()
+    print(request.base_url)
+    print()
+    print(request.headers['content-type'])
+    print()
+    body = b''
+    async for chunk in request.stream():
+        body += chunk
+        print(body)
+    print()
+    # print(dir(request))
     return json.dumps({'result': 'ok'})
